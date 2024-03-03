a74460fad4dea826101afc10bcbe4e5f99fb6f53
speedyHKjournalist <xingbizhou@gmail.com>
2024-03-03 20:01:36 +0800

    Clean code

--- app/src/main/java/openMapleClient/MapleActivity.kt
+++ app/src/main/java/openMapleClient/MapleActivity.kt
@@ -1,24 +1,7 @@
 package openMapleClient
 
 import android.app.NativeActivity
-import android.os.Bundle
-import android.view.Gravity
 
 
 class MapleActivity : NativeActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        SetLayoutMapleClient()
-    }
-
-    private fun SetLayoutMapleClient() {
-        val windowManager = windowManager
-        val display = windowManager.defaultDisplay
-        val screenHeight = display.height
-        val screenWidth = display.width
-        val params = window.attributes
-        params.width = screenWidth * 800 / screenHeight
-        params.gravity = Gravity.CENTER
-        window.attributes = params
-    }
 }
