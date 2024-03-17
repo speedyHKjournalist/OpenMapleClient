603ba6274feefe2d6c226bf1b7929ebc84abd064
speedyHKjournalist <xingbizhou@gmail.com>
2024-03-17 18:59:27 +0800

    avoid screen timeout

--- app/src/main/cpp/gl.cpp
+++ app/src/main/cpp/gl.cpp
@@ -1,6 +1,9 @@
 #include <gl.h>
+#include <android/window.h>
 #include "glfm.h"
 
 void glfmMain(GLFMDisplay *display) {
+    ANativeActivity_setWindowFlags((ANativeActivity *) glfmGetAndroidActivity(display),
+                                   AWINDOW_FLAG_KEEP_SCREEN_ON, 0);
     ms::game.start(display);
 }
